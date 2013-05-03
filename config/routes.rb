013910ba5d0c5b2f74e073e0ac3475a501c71c4e
Doug Gerber <dojoh@outlook.com>
2013-05-03 18:35:35 -0400

    Add Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Abapp::Application.routes.draw do
+
+root :to => 'pages#home'
+
+get 'about' => 'pages#about'
+get 'contact' => 'pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +54,6 @@ Abapp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
